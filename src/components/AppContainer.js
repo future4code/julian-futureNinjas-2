4a00b05af0cc5710ff912772ba619b0ffef0a8dc
NilsonKuroki <nilsonkurokii@gmail.com>
2020-05-06 17:26:30 -0300

    fiz os blocos de servicos contratados

--- src/components/AppContainer.js
+++ src/components/AppContainer.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
+import {ServicosContratados} from './ServicosContratados'
 
 export class AppContainer extends Component {
   render() {
     return (
       <div>
-
+        <ServicosContratados/>
       </div>
     )
   }
