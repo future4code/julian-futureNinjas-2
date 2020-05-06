f959d224db10e989b212f86de9fa8d6dc59f9814
NilsonKuroki <nilsonkurokii@gmail.com>
2020-05-06 15:49:40 -0300

    Foi feito um exatamente igual ao que foi pedido no trello, no lugar do amarelo, usei a cor secundary para que pudesse na hora da troca da cor, ela seja alterada

--- src/components/AppContainer.js
+++ src/components/AppContainer.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
+import {CardServicosCadastrados} from './CardServicosCadastrados'
 
 export class AppContainer extends Component {
   render() {
     return (
       <div>
-
+        <CardServicosCadastrados/>
       </div>
     )
   }
