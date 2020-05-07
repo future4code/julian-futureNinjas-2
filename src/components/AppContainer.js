ae6394f3c3267acd19bb58f01d79b8dcdae7180e
Hugo Lacerda <hugolacerdar@gmail.com>
2020-05-06 23:03:08 -0300

    Filtro e CardMin

--- src/components/AppContainer.js
+++ src/components/AppContainer.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react'
-
+import CardServiçoMin from './CardServiçoMin'
+import Filtro from './Filtro'
 export class AppContainer extends Component {
   render() {
     return (
       <div>
-
+      <Filtro/>
+        <CardServiçoMin/>
+        <CardServiçoMin/>
+        <CardServiçoMin/>
+        <CardServiçoMin/>
+        <CardServiçoMin/>
+        <CardServiçoMin/>
+        <CardServiçoMin/>
       </div>
     )
   }
