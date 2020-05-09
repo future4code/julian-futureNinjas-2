607c751041ef9fe61f0198ec528c918318707ca3
axpalx <axpalx@gmail.com>
2020-05-08 23:54:49 -0300

    Puxar card de servicos da API

--- src/components/BlocoServicoUsuario.js
+++ src/components/BlocoServicoUsuario.js
@@ -1,101 +1,74 @@
-import React from 'react'
-import styled from 'styled-components'
-import Button from '@material-ui/core/Button';
+import React from "react";
+import styled from "styled-components";
+import Button from "@material-ui/core/Button";
 import axios from "axios";
 
-
 const ContainerGeralBlocoServicoUsuario = styled.div`
-    display: flex;
-    justify-content: center;
-`
+  display: flex;
+  justify-content: center;
+`;
 const ContainerBlocoServicoUsuario = styled.div`
-    display: flex;
-    flex-direction: column;
-    width: 80vw;
-    border-radius: 10px;
-    background-color: #ECEBF2;
-`
-const ContainerTitulo =styled.div`
-    display: flex;
-    justify-content: center;
-    width: 100%;
-    background-color: pink;
-    border-radius: 10px;
-    background-color: #9B7ED9;
-`
+  display: flex;
+  flex-direction: column;
+  width: 80vw;
+  border-radius: 10px;
+  background-color: #ecebf2;
+`;
+const ContainerTitulo = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  background-color: pink;
+  border-radius: 10px;
+  background-color: #9b7ed9;
+`;
 const ContainerPrecoPrazo = styled.div`
-    display: flex;
-    justify-content: space-around;
-`
+  display: flex;
+  justify-content: space-around;
+`;
 const Descricao = styled.div`
-    background-color:  #F4F4F4;
-    height: 120px;
-    border-radius: 10px;
-    margin-right: 1%;
-    margin-left: 1%;   
-`
+  background-color: #f4f4f4;
+  height: 120px;
+  border-radius: 10px;
+  margin-right: 1%;
+  margin-left: 1%;
+`;
 const ButtonContainer = styled.div`
-    margin: 10px;
-    display: flex;
-    flex-direction: row-reverse;
-`
-export class BlocoServicoUsuario extends React.Component{
-    state = {
-        listaServicos: [
-            {    
-                id: [],
-                title: [],
-                description: [],
-                value: [],
-                dueDate: []
-            }
-        ]
-    }
+  margin: 10px;
+  display: flex;
+  flex-direction: row-reverse;
+`;
+export class BlocoServicoUsuario extends React.Component {
+  state = {
+    listaServicos: [],
+  };
 
-    componentDidMount() {
-        this.pegarServico();
-    }
-    
-    pegarServico = () => {
-        axios
-            .get(
-                `https://us-central1-labenu-apis.cloudfunctions.net/futureNinjasTwo/jobs`,
-            )
-            .then(servico => {
-                this.setState({ servico: servico.data });
-            })
-            .catch(error => {
-                console.log(error.response);
-            });
-    };
-
-    render(){
-        const contrataServico = this.state.listaServicos.map((servico) => {
-            console.log(servico.data)
-            return(
-                <ContainerGeralBlocoServicoUsuario>
-                    <ContainerBlocoServicoUsuario>
-                        <ContainerTitulo>
-                            <h2>{servico.title}</h2>
-                        </ContainerTitulo>
-                        <ContainerPrecoPrazo>
-                            <p>{servico.value}</p>
-                            <p>{servico.duedate}</p>
-                        </ContainerPrecoPrazo>
-                        <Descricao>
-                            <p>{servico.description}</p>
-                        </Descricao>
-                        <ButtonContainer>
-                            <Button variant="contained" color="primary">Contratar</Button>
-                        </ButtonContainer>
-                    </ContainerBlocoServicoUsuario>
-                </ContainerGeralBlocoServicoUsuario>
-            )
-        })
-        return(
-            <div>contrataServico</div>
-        )
-    }
+  render() {
+    const contrataServico = this.state.listaServicos.map((servico) => {
+      return (
+        <ContainerGeralBlocoServicoUsuario>
+          <ContainerBlocoServicoUsuario>
+            <ContainerTitulo>
+              <h2>{servico.title}</h2>
+            </ContainerTitulo>
+            <ContainerPrecoPrazo>
+              <p>{servico.value}</p>
+              <p>{servico.duedate}</p>
+            </ContainerPrecoPrazo>
+            <Descricao>
+              <p>{servico.description}</p>
+            </Descricao>
+            <ButtonContainer>
+              <Button variant="contained" color="primary">
+                Contratar
+              </Button>
+            </ButtonContainer>
+          </ContainerBlocoServicoUsuario>
+        </ContainerGeralBlocoServicoUsuario>
+      );
+    });
+    return <div>{contrataServico}</div>;
+  }
 }
 
 export default BlocoServicoUsuario;
--- src/components/CardServiçoMin.js
+++ src/components/CardServiçoMin.js
@@ -1,49 +1,86 @@
-import React, { Component } from 'react'
-import { Card, TextField, Button, Typography } from '@material-ui/core'
-import styled from 'styled-components'
+import React, { Component } from "react";
+import { Card, TextField, Button, Typography } from "@material-ui/core";
+import styled from "styled-components";
+import BlocoServicoUsuario from "./BlocoServicoUsuario";
+import axios from "axios";
 
 const CartaoMin = styled(Card)`
-display: flex;
-justify-content: space-between;
-align-items: center;
-padding: 10px 20px;
-margin: 20px 320px;
-background-color: #ECEBF2;
-color: #404040;
-*{
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px 20px;
+  margin: 20px 320px;
+  background-color: #ecebf2;
+  color: #404040;
+  * {
     color: inherit;
-}
-height: 70px;
-`
+  }
+  height: 70px;
+`;
 const TituloServ = styled(Typography)`
-font-weight: bold;
-width: 30%;
-align-self: center;
-font-size: 1.3rem;
-`
+  font-weight: bold;
+  width: 30%;
+  align-self: center;
+  font-size: 1.3rem;
+`;
 const TextoComp = styled(Typography)`
-font-size: 1.2rem;
-width: 20%;
-`
+  font-size: 1.2rem;
+  width: 20%;
+`;
 const BotaoVisualizar = styled(Button)`
-background-color: #8D6AD9;
-color: #ECEBF2;
-padding: 5px 20px;
-width: 15%;
+  background-color: #8d6ad9;
+  color: #ecebf2;
+  padding: 5px 20px;
+  width: 15%;
 
-font-weight: bold;
-`
+  font-weight: bold;
+`;
 
 export default class CardServiçoMin extends Component {
+  state = {
+    servicos: [],
+  };
+
+  aparecerBlocoServicoUsuario = () => {
+    this.setState({ menu: "servicos usuario" });
+  };
+
+  componentDidMount() {
+    this.preencherCardsServicos();
+  }
+  preencherCardsServicos = () => {
+    axios
+      .get(
+        "https://us-central1-labenu-apis.cloudfunctions.net/futureNinjasTwo/jobs"
+      )
+      .then((response) => {
+        const dadosApi = response.data.jobs;
+        this.setState({ servicos: dadosApi });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
   render() {
-    return (
-      <CartaoMin>
-          <TituloServ variant='h5'>{this.props.servico.title}</TituloServ>
-          <TextoComp>R$ {this.props.servico.value}</TextoComp>
-          <TextoComp>Prazo: {this.props.servico.dueDate}</TextoComp>
-        <BotaoVisualizar onClick={this.props.onVisualizarClick}>Visualizar</BotaoVisualizar>
-      </CartaoMin>
-    )
+    const listaDeServicos = this.state.servicos.map((servico) => {
+      return (
+        <div>
+          {this.state.menu === "servicos usuario" ? (
+            <BlocoServicoUsuario />
+          ) : (
+            <CartaoMin>
+              <TituloServ variant="h5">{servico.title}</TituloServ>
+              <TextoComp>R$ {servico.value}</TextoComp>
+              <TextoComp>Prazo: {servico.dueDate}</TextoComp>
+              <BotaoVisualizar onClick={() => this.aparecerBlocoServicoUsuario}>
+                Visualizar
+              </BotaoVisualizar>
+            </CartaoMin>
+          )}
+        </div>
+      );
+    });
+    return <div>{listaDeServicos}</div>;
   }
 }
